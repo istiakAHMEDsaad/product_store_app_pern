6c74cd9fc331bb8394864cbbf35e9afc51038829
istiakAHMEDsaad <istik2020@gmail.com>
2026-01-09 17:19:10 +0600

    added users, products, comments schema

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,7 +1,41 @@
 import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
-import { Relation } from 'drizzle-orm';
+import { relations } from 'drizzle-orm';
 
-export const user = pgTable('users', {
-  id: text('id').primaryKey(),
+// key is got on the code, value save on the database
+export const users = pgTable('users', {
+  id: text('id').primaryKey(), //clerk id
   email: text('email').notNull().unique(),
+  name: text('name'),
+  imageUrl: text('image_url'),
+  createdAt: timestamp('created_at', { mode: 'date' }).notNull().defaultNow(),
+  updatedAt: timestamp('updated_at', { mode: 'date' }).notNull().defaultNow(),
 });
+
+export const products = pgTable('products', {
+  id: uuid('id').defaultRandom().primaryKey(), // generate auto for database
+  title: text('title').notNull(),
+  description: text('description').notNull(),
+  imageUrl: text('image_url').notNull(),
+  // cascade -> if a user delete user account all product also delete
+  userId: text('user_id')
+    .notNull()
+    .references(() => users.id, { onDelete: 'cascade' }),
+  createdAt: timestamp('created_at', { mode: 'date' }).notNull().defaultNow(),
+  updatedAt: timestamp('updated_at', { mode: 'date' }).notNull().defaultNow(),
+});
+
+export const comments = pgTable('comments', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  content: text('content').notNull(),
+  userId: text('user_id')
+    .notNull()
+    .references(() => users.id, { onDelete: 'cascade' }),
+  product_Id: text('product_id')
+    .notNull()
+    .references(() => products.id, { onDelete: 'cascade' }),
+  createdAt: timestamp('created_at', { mode: 'date' }).notNull().defaultNow(),
+  updatedAt: timestamp('updated_at', { mode: 'date' }).notNull().defaultNow(),
+});
+
+// Relation define how tables connect each other. This enables Drizzle's query API
+// to automatically join related data when using `with: {relationName: true}`
