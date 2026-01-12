8520fc4e4d961fc29fad12296f5a840ef69ba6b4
istiakAHMEDsaad <istik2020@gmail.com>
2026-01-13 01:10:27 +0600

    added routes for each operation

--- backend/src/routes/productRoutes.ts
+++ backend/src/routes/productRoutes.ts
@@ -5,5 +5,21 @@ import { requireAuth } from '@clerk/express';
 const router = Router();
 
 // GET /api/products => get all products
-router.post('/', productController.getAllProducts);
+router.get('/', productController.getAllProducts);
+
+// GET /api/products/my-products => get users all product (protected)
+router.get('/my-products', requireAuth(), productController.getMyProducts);
+
+// GET /api/products/:id => get single product by id (public)
+router.get('/:id', productController.getProductById);
+
+// POST /api/products => create new product (protected)
+router.post('/', requireAuth(), productController.createProduct);
+
+// PUT /api/products/:id => update product (protected)
+router.put('/:id', requireAuth(), productController.updateProduct);
+
+// DELETE /api/products/:id => delete product (protected)
+router.delete('/:id', requireAuth(), productController.deleteProduct);
+
 export default router;
